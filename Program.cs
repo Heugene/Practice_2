3f53762f757b2687a93cedef295d6b17c0373bc2
Yevhenii Heoka@git config --global user.name Yevhenii <109804875+Heugene@users.noreply.github.com>
2023-12-18 18:17:15 +0200

    Implemented Query 10

--- Program.cs
+++ Program.cs
@@ -209,14 +209,16 @@ namespace Practice_Linq
         {
             //Query 10: Вивести з 5-го по 10-тий (включно) матчі Gold Cup, які відбулися у липні 2023 р.
 
-            var selectedGames = games;    // Корегуємо запит !!!
+            var selectedGames = games.Where(game => game.Date.Year == 2023 && game.Date.Month == 7 && game.Tournament == "Gold Cup").OrderBy(game => game.Date).Take(10).Skip(4);
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 10 ========================");
 
             // див. приклад як має бути виведено:
-
-
+            foreach (var game in selectedGames)
+            {
+                Console.WriteLine($"{game.Date.ToShortDateString()} {game.Home_team} - {game.Away_team}, Score: {game.Home_score} - {game.Away_score}, Country: {game.Country}");
+            }
         }
 
     }
