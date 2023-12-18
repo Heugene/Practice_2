a1fb3986d7772bb0374cc75645ec5caf0b4bcbcc
Yevhenii Heoka@git config --global user.name Yevhenii <109804875+Heugene@users.noreply.github.com>
2023-12-18 13:09:56 +0200

    Implemented Query 4

--- Program.cs
+++ Program.cs
@@ -101,15 +101,17 @@ namespace Practice_Linq
         {
             //Query 4: Вивести всі матчі збірної Германії з 2018 року по 2020 рік (включно), в яких вона на виїзді програла.
 
-            var selectedGames = games;   // Корегуємо запит !!!
+            var selectedGames = games.Where(game => game.Away_team == "Germany" && game.Home_score - game.Away_score > 0 && game.Date.Year >= 2018 && game.Date.Year <= 2020);
 
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 4 ========================");
 
             // див. приклад як має бути виведено:
-
-
+            foreach (var game in selectedGames)
+            {
+                Console.WriteLine($"{game.Date.ToShortDateString()} {game.Home_team} - {game.Away_team}, Score: {game.Home_score} - {game.Away_score}, Country: {game.Country}");
+            }
         }
 
         // Запит 5
